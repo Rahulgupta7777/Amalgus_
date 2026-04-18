0f55fe0959b5ee1bef9998578c6dddfb9cc5f060
Rahul Gupta <rahulgupta193246@gmail.com>
2026-04-18 08:16:56 +0530

    Update lib/constants.ts

---
+++ lib/constants.ts
@@ -0,0 +1,11 @@
+export const GST_RATE = 0.18
+export const MAX_QUERY_LEN = 500
+export const MIN_QUERY_LEN = 5
+export const MM_TO_SQFT = 92903
+
+export const ROLES = ['Homeowner', 'Architect', 'Builder', 'Dealer'] as const
+export const DEFAULT_ROLE = 'Homeowner'
+
+export const API_TIMEOUT_MS = 10000
+export const API_FALLBACK_MODEL = 'llama-3.1-8b-instant'
+export const API_PRIMARY_MODEL = 'llama-3.3-70b-versatile'
